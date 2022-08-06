9747b5081e57737c4096a0e9fbd08d115d92cde2
Janice <janiceralecha@gmail.com>
2022-08-06 14:36:48 -0500

    commit tree 0b071d472ecfc2df2357ea9a643c76456092cc71

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
-// Code Keypad Component Here
+/*In the components/Keypad.js file, create a Keypad React component.
+In that component, render an input with the right type.
+On that input, add an event handler that listens for the change event.
+When that event fires, use console.log to print out the text 'Entering password...'.*/
 
 function Keypad (){
+    function handleChange(event) {
+        event.preventDefault();
+        console.log(`Entering password...`);
+      }
     return (
-        <div></div>
-    )
+        <>
+            <input type="password" onChange={handleChange} />
+        </>
+    );
 }
 
 export default Keypad;
